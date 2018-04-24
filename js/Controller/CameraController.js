826015f2445b848ba047dd6196327246a1fbaad2
Florian Wedemeier <florian_wedemeier@web.de>
2018-04-24 22:37:14 +0200

    Controller Update Mouse

--- js/Controller/CameraController.js
+++ js/Controller/CameraController.js
@@ -4,7 +4,9 @@ class CameraController
 
 		document.addEventListener("keydown", (e) => {this.move(e);});
 		document.addEventListener("keyup", (e) => {this.stop(e);});
-		document.addEventListener("mousemove", (e) => {this.mouse(e);});
+		document.addEventListener("mousedown", (e) => {this.mouseDown(e);});
+		document.addEventListener("mouseup", (e) => {this.mouseUp(e);});
+		document.addEventListener("mousemove", (e) => {this.mouseMove(e);});
 		this.moveForward = false;
 		this.moveBackward = false;
 		this.camera = camera;
@@ -35,16 +37,45 @@ class CameraController
 		}
 	}
 
-	mouse(evt) {
-		console.log(evt.clientX);		
+	mouseDown(evt) {
+		var button = evt.which;
+		switch(button){
+			case 1:
+				this.leftMouseDown = true;
+				break;
+			case 2: 
+				this.rightMouseDown = true;	
+				break;
+		}
 	}
+
+	mouseUp(evt) {
+		var button = evt.which;
+		switch(button){
+			case 1:
+				this.leftMouseDown = false;
+				break;
+			case 2: 
+				this.rightMouseDown = false;	
+				break;
+		}
+	}
+
+	mouseMove(evt) {
+		this.mouseMoving = true;
+		this.clientX = evt.clientX;
+	}
+
 
 	action() {
 		if (this.moveForward)
 			camera.move(0.1);
 		if (this.moveBackward)
 			camera.move(-0.1);
-		
+
+		if (this.mouseMoving && this.leftMouseDown)
+			console.log(this.clientX);		
+
 		gl.useProgram(this.shader);
 	    gl.uniformMatrix4fv(gl.getUniformLocation(this.shader, "gPerspective"), gl.FALSE, this.camera.perspectiveMatrix);
 	    gl.uniformMatrix4fv(gl.getUniformLocation(this.shader, "gWorldToCamera"), gl.FALSE, this.camera.worldToCameraMatrix);
